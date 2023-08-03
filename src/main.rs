27c6c27776e0a8bbb8c2ae6f3ab401506ca850f4
prongbang <prongbang@gmail.com>
2023-08-03 23:16:56 +0700

    Initial

---
+++ src/main.rs
@@ -0,0 +1,32 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    platform: String,
+
+    #[arg(short, long, default_value = "")]
+    config: String,
+
+    #[arg(long, default_value = "")]
+    flavor: String,
+
+    #[arg(short, long, default_value = "")]
+    device: String,
+
+    #[arg(short, long, default_value = "")]
+    file: String,
+
+    #[arg(short, long, default_value = "")]
+    report: String,
+
+    #[arg(short, long, default_value = "integration_test")]
+    r#type: String,
+}
+
+fn main() {
+    let args = Args::parse();
+
+    println!("{:?}", args)
+}
