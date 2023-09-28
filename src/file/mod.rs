5203294f31dbc92618206284365d58806fe1647b
prongbang <prongbang@gmail.com>
2023-09-28 22:46:10 +0700

    Expect result create dir

--- src/file/mod.rs
+++ src/file/mod.rs
@@ -12,7 +12,7 @@ pub fn check_or_create_is_not_exist_dir(dir: &str) {
             // Check if the directory exists
             if !fs::metadata(dir_str).is_ok() {
                 // If it doesn't exist, create it
-                fs::create_dir(dir_str)?;
+                fs::create_dir(dir_str).expect("Cannot create directory");
             }
         }
     } else {
